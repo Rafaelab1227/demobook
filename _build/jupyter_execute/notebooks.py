d8f535bb60efaa9db52625372059a4ef522d8e8f
Rb <rbecerra@di.uc3m.es@jupyterhub.uc3m.es>
2021-04-06 17:37:53 +0000

    Update files

--- _build/jupyter_execute/notebooks.py
+++ _build/jupyter_execute/notebooks.py
@@ -1,65 +1,119 @@
-# Content with notebooks
+# Wiki de ejemplos
 
-You can also create content with Jupyter Notebooks. This means that you can include
-code blocks and their outputs in your book.
-
-## Markdown + notebooks
-
-As it is markdown, you can embed images, HTML, etc into your posts!
-
-![](https://myst-parser.readthedocs.io/en/latest/_static/logo.png)
-
-You an also $add_{math}$ and
-
-$$
-math^{blocks}
-$$
-
-or
-
-$$
-\begin{aligned}
-\mbox{mean} la_{tex} \\ \\
-math blocks
-\end{aligned}
-$$
-
-But make sure you \$Escape \$your \$dollar signs \$you want to keep!
-
-## MyST markdown
-
-MyST markdown works in Jupyter Notebooks as well. For more information about MyST markdown, check
-out [the MyST guide in Jupyter Book](https://jupyterbook.org/content/myst.html),
-or see [the MyST markdown documentation](https://myst-parser.readthedocs.io/en/latest/).
-
-## Code blocks and outputs
-
-Jupyter Book will also embed your code blocks and output in your book.
-For example, here's some sample Matplotlib code:
-
-from matplotlib import rcParams, cycler
-import matplotlib.pyplot as plt
-import numpy as np
-plt.ion()
-
-# Fixing random state for reproducibility
-np.random.seed(19680801)
-
-N = 10
-data = [np.logspace(0, 1, 100) + np.random.randn(100) + ii for ii in range(N)]
-data = np.array(data).T
-cmap = plt.cm.coolwarm
-rcParams['axes.prop_cycle'] = cycler(color=cmap(np.linspace(0, 1, N)))
+En esta sección se puede encontrar una galeria de Jupyter Notebooks como ejemplo de su uso basándonos en patrones pedagógicos.
 
 
-from matplotlib.lines import Line2D
-custom_lines = [Line2D([0], [0], color=cmap(0.), lw=4),
-                Line2D([0], [0], color=cmap(.5), lw=4),
-                Line2D([0], [0], color=cmap(1.), lw=4)]
-
-fig, ax = plt.subplots(figsize=(10, 5))
-lines = ax.plot(data)
-ax.legend(custom_lines, ['Cold', 'Medium', 'Hot']);
-
-There is a lot more that you can do with outputs (such as including interactive outputs)
-with your book. For more information about this, see [the Jupyter Book documentation](https://jupyterbook.org)
+%%html
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg .tg-lboi{border-color:inherit;text-align:left;vertical-align:middle}
+.tg .tg-c094{background-color:#3D8EB9;border-color:inherit;text-align:center;vertical-align:middle}
+.tg .tg-9wq8{border-color:inherit;text-align:center;vertical-align:middle}
+.tg .tg-b9ha{border-color:inherit;color:#000000;text-align:left;vertical-align:middle}
+.tg .tg-rcip{background-color:#FFF;border-color:inherit;text-align:center;vertical-align:middle}
+@media screen and (max-width: 767px) {.tg {width: auto !important;}.tg col {width: auto !important;}.tg-wrap {overflow-x: auto;-webkit-overflow-scrolling: touch;}}</style>
+<div class="tg-wrap"><table class="tg" style="undefined;table-layout: fixed; width: 1167px">
+<colgroup>
+<col style="width: 254px">
+<col style="width: 448px">
+<col style="width: 465px">
+</colgroup>
+<thead>
+  <tr>
+    <th class="tg-c094"><span style="background-color:#3D8EB9">Patrón pedagógico</span></th>
+    <th class="tg-c094"><span style="background-color:#3D8EB9">Descripción</span></th>
+    <th class="tg-c094"><span style="background-color:#3D8EB9">Ejemplo</span></th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td class="tg-9wq8">Shift-Enter</td>
+    <td class="tg-9wq8">Los alumnos leen y ejecutan código, así como también interactúan con widgets para explorar conceptos.</td>
+    <td class="tg-lboi"><a href="https://nbviewer.jupyter.org/github/lmarti/evolutionary-computation-course/blob/master/AEC.06%20-%20Evolutionary%20Multi-Objective%20Optimization.ipynb" target="_blank" rel="noopener noreferrer">Advanced Evolutionary Computation: Theory and Practice course por Luis Martí. Understanding evolutionary strategies and covariance matrix adaptation. Evolutionary Multi-Objective Optimization</a></td>
+  </tr>
+  <tr>
+    <td class="tg-9wq8">Completar espacios</td>
+    <td class="tg-9wq8">Los alumnos deberán completar partes de código faltantes para generar resultados que corroborarán su respuesta.</td>
+    <td class="tg-lboi"><a href="https://nbviewer.jupyter.org/github/mbakker7/exploratory_computing_with_python/blob/master/notebook1_basics_plotting/py_exploratory_comp_1_sol.ipynb" target="_blank" rel="noopener noreferrer">Exploratory computing with Python por Mark Bakker de Delft University of Technology. Notebook 1: Basics and Plotting</a></td>
+  </tr>
+  <tr>
+    <td class="tg-9wq8">Práctica objetivo</td>
+    <td class="tg-9wq8">Se proporciona todos los pasos del flujo de trabajo, excepto el que es el foco del ejercicio, el cual deberá ser resuelto por el estudiante.</td>
+    <td class="tg-lboi"><a href="https://nbviewer.jupyter.org/github/mbakker7/exploratory_computing_with_python/blob/master/notebook4_functions/py_exploratory_comp_4_sol.ipynb" target="_blank" rel="noopener noreferrer">Exploratory computing with Python por Mark Bakker de Delft University of Technology. Notebook 4: Functions</a></td>
+  </tr>
+  <tr>
+    <td class="tg-9wq8">Tweak, twiddle, and frob</td>
+    <td class="tg-9wq8">Los alumnos reciben un cuaderno con un ejemplo, deberán leer el texto, ejecutar el código y analizar los resultados. Posteriormente, deberán realizar cambios pequeños, ligeros, o substanciales y correr nuevamente el código para comparar resultados.</td>
+    <td class="tg-b9ha"><a href="https://nbviewer.jupyter.org/github/lmarti/evolutionary-computation-course/blob/master/AEC.04%20-%20Evolutionary%20Strategies%20and%20Covariance%20Matrix%20Adaptation.ipynb" target="_blank" rel="noopener noreferrer">Advanced Evolutionary Computation: Theory and Practice course por Luis Martí. Understanding evolutionary strategies and covariance matrix adaptation</a></td>
+  </tr>
+  <tr>
+    <td class="tg-9wq8">Notebooks como aplicaciones</td>
+    <td class="tg-9wq8">Se crean cuadernos con interfaces haciendo uso de herramientas como controles deslizantes, cuadros de entrada y botones de alternancia para generar interacción.</td>
+    <td class="tg-lboi">1. <a href="https://mybinder.org/v2/gh/ipython-books/cookbook-2nd-code/master" target="_blank" rel="noopener noreferrer">Code of the IPython Cookbook, Second Edition (2018), Chapter 3</a><br>2.<a href="https://nbviewer.jupyter.org/gist/fonnesbeck/8495259" target="_blank" rel="noopener noreferrer">Using Plotly for Interactive and Collaborative Data Visualization</a>¶<br>3. <a href="https://nbviewer.jupyter.org/github/maojrs/ipynotebooks/blob/master/acoustics_riemann.ipynb" target="_blank" rel="noopener noreferrer">Acoustics equations and their Riemann solver in 1D and 2D by Mauricio J. Del Razo S. 20141</a></td>
+  </tr>
+  <tr>
+    <td class="tg-9wq8">Win-day-one</td>
+    <td class="tg-9wq8">Se muestra la respuesta rápida y concisa del problema a través de un cuaderno que se denomina "win", posteriormente, se despliegan los demás cuadernos mostrando todas las fases de resolución.</td>
+    <td class="tg-lboi"><a href="https://nbviewer.jupyter.org/github/jdwittenauer/ipython-notebooks/blob/master/notebooks/fastai/Fastai-Lesson1.ipynb" target="_blank" rel="noopener noreferrer">Lesson 1 - Image Classification by Notebooks from Jeremy Howard's fast.ai class.</a></td>
+  </tr>
+  <tr>
+    <td class="tg-9wq8">Top-down sequence</td>
+    <td class="tg-9wq8">Se presenta primero la utilidad de la herramienta para luego mostrar cómo funciona, lo que hace que sea más atractivo y motivante para el estudiante.</td>
+    <td class="tg-lboi"><a href="https://github.com/dmnguyen92/The-collapse-of-bubble-near-a-wall/blob/master/Bubble%20collapse%20near%20a%20wall.ipynb" target="_blank" rel="noopener noreferrer">The-collapse-of-bubble-near-a-wall</a></td>
+  </tr>
+  <tr>
+    <td class="tg-rcip"><span style="color:#000;background-color:#FFF">Codificación como traducción</span></td>
+    <td class="tg-9wq8">Se toma explícitamente ecuaciones matemáticas y se las traduce en código.</td>
+    <td class="tg-lboi">1. <a href="https://nbviewer.jupyter.org/github/CamDavidsonPilon/Probabilistic-Programming-and-Bayesian-Methods-for-Hackers/blob/master/Chapter1_Introduction/Ch1_Introduction_PyMC3.ipynb" target="_blank" rel="noopener noreferrer">Probabilistic-Programming-and-Bayesian-Methods-for-Hackers</a><br>2. <a href="https://nbviewer.jupyter.org/github/jckantor/CBE20255/blob/master/notebooks/01.01-Units-and-Engineering-Calculations.ipynb" target="_blank" rel="noopener noreferrer">CBE20255 Introduction to Chemical Engineering Analysis</a></td>
+  </tr>
+  <tr>
+    <td class="tg-9wq8">Remplazar análisis con métodos numéricos</td>
+    <td class="tg-9wq8">Promover el uso de simulaciones y métodos numéricos en lugar de solo realizar análisis.</td>
+    <td class="tg-lboi"><a href="https://krischer.github.io/seismo_live_build/html/Earthquake%20Physics/rupturedynamics_2D/rupture_2D_wrapper.html" target="_blank" rel="noopener noreferrer">Seismo-Live Live Jupyter Notebooks for Seismology</a></td>
+  </tr>
+  <tr>
+    <td class="tg-9wq8">Prueba por ejemplo, refutación por contraejemplo</td>
+    <td class="tg-9wq8">Mediante ejemplos los alumnos pueden experimentar resultados generales derivados o probados y ver cómo funcionan y cuándo fallan.</td>
+    <td class="tg-lboi"><a href="https://nbviewer.jupyter.org/github/nealcaren/workshop_2014/blob/master/notebooks/3_Sentiment.ipynb" target="_blank" rel="noopener noreferrer">Workshop on text analysis by Neal Caren. Sentiment Analysis</a></td>
+  </tr>
+  <tr>
+    <td class="tg-9wq8">El mundo es tu base de datos</td>
+    <td class="tg-9wq8">Presentar notebooks conectados a data de fuentes externas permite al alumno trabajar directamente con un mayor número de recursos.</td>
+    <td class="tg-lboi"><a href="https://colab.research.google.com/github/jakevdp/PythonDataScienceHandbook/blob/master/notebooks/03.09-Pivot-Tables.ipynb#scrollTo=cMR6decmYqtu" target="_blank" rel="noopener noreferrer">Python Data Science Handbook por Jake VanderPlas</a></td>
+  </tr>
+  <tr>
+    <td class="tg-9wq8">Tu turno</td>
+    <td class="tg-9wq8">El estudiante crea un cuaderno nuevo en base a uno dado por el instructor como una variación o un ejemplo similar al dado.</td>
+    <td class="tg-lboi"><a href="https://nbviewer.jupyter.org/github/lmarti/evolutionary-computation-course/blob/master/AEC.04%20-%20Evolutionary%20Strategies%20and%20Covariance%20Matrix%20Adaptation.ipynb" target="_blank" rel="noopener noreferrer">Understanding evolutionary strategies and covariance matrix adaptation¶Luis Martí, IC/UFF</a></td>
+  </tr>
+  <tr>
+    <td class="tg-9wq8">Crear contenido para la comunidad</td>
+    <td class="tg-9wq8">Fomentar en el estudiante el uso de data externa y compartir su trabajo en plataformas para que otros puedan reusarlas y revisarlas.</td>
+    <td class="tg-lboi"><a href="http://readiab.org/" target="_blank" rel="noopener noreferrer">An Introduction to Applied Bioinformatics</a></td>
+  </tr>
+  <tr>
+    <td class="tg-9wq8">Competiciones</td>
+    <td class="tg-9wq8">Crear competiciones y fomentar la participación de los estudiantes en competiciones con el uso del ecosistema reproducible de Jupyter para que refuercen ss conocimientos.</td>
+    <td class="tg-lboi"><a href="https://www.kaggle.com/c/reservas-de-hotel/overview" target="_blank" rel="noopener noreferrer">Kaggle Reservas Hoteles</a><br><a href="https://nbviewer.jupyter.org/github/agconti/kaggle-titanic/blob/master/Titanic.ipynb" target="_blank" rel="noopener noreferrer">Titanic</a></td>
+  </tr>
+  <tr>
+    <td class="tg-9wq8">Tests y revisión automática de códigos</td>
+    <td class="tg-9wq8">Herramientas adicionales permiten crear directamente tests con autocalificación y feedback inmediato para el alumno.</td>
+    <td class="tg-lboi"><a href="https://nbgrader.readthedocs.io/en/stable/index.html" target="_blank" rel="noopener noreferrer">Nbgrader documentation</a><br><a href="https://datamine.purdue.edu/seminars/spring2020/stat19000project01.html%20Se%20ha%20habilitado%20la%20compatibilidad%20con%20lectores%20de%20pantalla." target="_blank" rel="noopener noreferrer">STAT 19000 Project 1 using Github classroom</a></td>
+  </tr>
+  <tr>
+    <td class="tg-9wq8">Identificación de errores</td>
+    <td class="tg-9wq8">El instructor crea notebooks con errores que deberán ser identificados por el alumno para poder correr correctamente los códigos.</td>
+    <td class="tg-lboi"><a href="https://nbviewer.jupyter.org/github/mbakker7/exploratory_computing_with_python/blob/master/notebook7_bugs/py_exploratory_comp_7_sol.ipynb" target="_blank" rel="noopener noreferrer">Exploratory computing with Python por Mark Bakker de Delft University of Technology</a></td>
+  </tr>
+  <tr>
+    <td class="tg-9wq8">Programación contradictoria</td>
+    <td class="tg-9wq8">Se crean notebooks separados de la función para resolver un problema y otros con pruebas para testear la función. Así se fomenta el pensamiento crítico en el alumno para ver todas las posibles fallas que pueden romper la solución planteada.</td>
+    <td class="tg-lboi"><a href="https://geo-python.github.io/site/develop/notebooks/L4/functions.html" target="_blank" rel="noopener noreferrer">Creating functions and test them</a></td>
+  </tr>
+</tbody>
+</table></div>
--- content.md
+++ content.md
@@ -1,5 +1,120 @@
-Content in Jupyter Book
+Funcionalidades
 =======================
 
-There are many ways to write content in Jupyter Book. This short section
-covers a few tips for how to do so.
+Esta herramiente permiten al usuario conjugar datos, código y prosa para poder contar una historia computacional interactiva, sumando la interactividad de una aplicación a las explicaciones.
+
+## Beneficios 
+
+Jupyter Notebooks promueve un amplio rango de metas de aprendizaje entre ellas el aprende a programar, incrementar el dominio del conocimiento, y practicar skills de comunicación como el "story telling". Específicamente se pueden encontrar varios beneficios asociados a cada tipo de usuario que han sido corroborados por [Teaching and Learning with Jupyter](https://jupyter4edu.github.io/jupyter-edu-book/why-we-use-jupyter-notebooks.html#course-benefits-anecdotes)
+
+%%html
+<style>
+.column {
+  float: left;
+  width: 33.33%;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+</style>
+
+<div class="row">
+  <div class="column"><style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg .tg-snc9{background-color:#3d8eb9;color:#ffffff;text-align:center;vertical-align:bottom}
+.tg .tg-8d8j{text-align:center;vertical-align:bottom}
+</style>
+<table class="tg">
+<thead>
+  <tr>
+    <th class="tg-snc9">Beneficios del curso</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td class="tg-8d8j">Compromiso</td>
+  </tr>
+  <tr>
+    <td class="tg-8d8j">Participación</td>
+  </tr>
+  <tr>
+    <td class="tg-8d8j">Mayor comprensión</td>
+  </tr>
+  <tr>
+    <td class="tg-8d8j">Incrementar el desempeño de los estudiantes</td>
+  </tr>
+</tbody>
+</table></div>
+  <div class="column"><style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg .tg-h1zv{background-color:#3d8eb9;color:#ffffff;text-align:left;vertical-align:bottom}
+.tg .tg-8d8j{text-align:center;vertical-align:bottom}
+</style>
+<table class="tg">
+<thead>
+  <tr>
+    <th class="tg-h1zv">Beneficios para estudiantes</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td class="tg-8d8j">Desarolla el pensamiento computacional</td>
+  </tr>
+  <tr>
+    <td class="tg-8d8j">Open Source</td>
+  </tr>
+  <tr>
+    <td class="tg-8d8j">Promueve el aprendizaje activo</td>
+  </tr>
+</tbody>
+</table></div>
+  <div class="column"><style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg .tg-snc9{background-color:#3d8eb9;color:#ffffff;text-align:center;vertical-align:bottom}
+.tg .tg-baqh{text-align:center;vertical-align:top}
+.tg .tg-8d8j{text-align:center;vertical-align:bottom}
+</style>
+
+<table class="tg">
+<thead>
+  <tr>
+    <th class="tg-snc9">Beneficios para instructor</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td class="tg-8d8j">Flexibilidad</td>
+  </tr>
+  <tr>
+    <td class="tg-8d8j">Notas de clase publicables e interactivas</td>
+  </tr>
+  <tr>
+    <td class="tg-baqh">Reutilización y adaptación de los recursos existentes</td>
+  </tr>
+  <tr>
+    <td class="tg-8d8j">Uso varios lenguajes</td>
+  </tr>
+  <tr>
+    <td class="tg-8d8j">Infraestructura en la nube</td>
+  </tr>
+</tbody>
+</table>
+</div>
+</div>
+
